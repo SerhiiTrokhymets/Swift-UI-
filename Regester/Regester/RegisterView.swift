88713c719b4984c5a20283034ff1f936a2f288f7
Serhii Trokhymets <serheyfly4410@gmail.com>
2020-07-09 20:02:11 +0300

    Alerts Lesson
    Program Alerts

---
+++ Regester/Regester/RegisterView.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterView.swift
+//  Regester
+//
+//  Created by Horizon on 07.07.2020.
+//  Copyright © 2020 Horizon. All rights reserved.
+//
+
+import UIKit
+
+class RegisterView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Regester/Regester/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  Regester
+//
+//  Created by Horizon on 06.07.2020.
+//  Copyright © 2020 Horizon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var LabelWelcome: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+  
+        
+
+    
+    @IBAction func Register(_ sender: UIButton) {
+        let alertRegister = UIAlertController(title: "Регистрация", message: "Введите ваши данние", preferredStyle: .alert)
+        let buttonRegister = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let buttonCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        
+        alertRegister.addAction(buttonRegister)
+        alertRegister.addAction(buttonCancel)
+        present(alertRegister, animated:  true, completion: nil)
+        
+        alertRegister.addTextField { (text) in
+            text.placeholder = "Ваше имя"            }
+        alertRegister.addTextField { (text) in
+            text.placeholder = "Введите ваш Email"   }
+        alertRegister.addTextField { (text) in
+            text.isSecureTextEntry = true
+            text.placeholder = "Введите ваш пароль"  }
+        alertRegister.addTextField { (text) in
+            text.isSecureTextEntry = true
+            text.placeholder = "Повторите ваш пароль" }
+        
+    }
+    
+    
+    @IBAction func LoginIn(_ sender: UIButton) {
+        let alertLogin = UIAlertController(title: "Вход", message: "Введите ваши данние", preferredStyle: .alert)
+        let buttonLogin = UIAlertAction (title: "OK", style: .default) { (action) in
+            let text = alertLogin.textFields?.first
+            self.LabelWelcome.text! += ("Добро пожаловать" + " ") + (text?.text!)!
+        }
+        let buttonCancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertLogin.addAction(buttonCancel)
+        alertLogin.addAction(buttonLogin)
+        present(alertLogin, animated: true, completion: nil)
+        
+        alertLogin.addTextField { (text) in
+            text.placeholder = "Ваш имя" }
+        alertLogin.addTextField { (text) in
+            text.isSecureTextEntry = true
+            text.placeholder = "Введите ваш пароль"
+        }
+        LabelWelcome.text = "Welcome"
+        LabelWelcome.text = alertLogin.textFields?.first?.text
+       
+    }
+    
+    
+    
+    
+}
+
